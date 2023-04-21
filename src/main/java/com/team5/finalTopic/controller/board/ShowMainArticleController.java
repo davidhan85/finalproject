0065890166dbbb68b0ac9eaf9f62618bc7e5f413
yuantenghsu <ss120785706@gmail.com>
2023-04-21 16:00:30 +0800

    20230421boardfront

--- src/main/java/com/team5/finalTopic/controller/board/ShowMainArticleController.java
+++ src/main/java/com/team5/finalTopic/controller/board/ShowMainArticleController.java
@@ -1,8 +1,13 @@
 package com.team5.finalTopic.controller.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShowMainArticleController {
 
+	@GetMapping("/board")
+	public String showMainArticles() {
+		return "/board/ShowMainArticles";
+	}
 }
